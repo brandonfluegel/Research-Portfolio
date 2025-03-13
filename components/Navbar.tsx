d691878f0e513c10173f85d580d3de57df45c045
Brandon Fluegel <Brandon.fluegel@js.held.com>
2025-03-12 21:05:36 -0600

    Fixed ESLint errors in NASAProjectSection and Navbar (unused imports, unescaped entities)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import useScrollDirection from "@/app/hooks/useScrollDirection";
 
 export default function Navbar() {
