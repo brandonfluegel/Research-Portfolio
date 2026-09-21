b6caa4580a8c932ef90f84caf651996791b9587b
Brandon <brandon.uxr@gmail.com>
2026-09-21 15:06:11 +0000

    Upgrade dependencies and finalize EchoStar branding

--- components/sections/projects/EchoStarProjectSection.tsx
+++ components/sections/projects/EchoStarProjectSection.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect, useSyncExternalStore } from 'react';
 import { m } from "framer-motion";
-import LogoBadge from "@/components/ui/LogoBadge";
 import { fadeInFromLeft, fadeInFromRight, staggerContainer } from "@/lib/utils/animationVariants";
 import {
   ScatterChart,
